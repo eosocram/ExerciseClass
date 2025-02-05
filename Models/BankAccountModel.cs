ddf14123523a5d11434210f916869e9829d829e5
Eosocram <socramlmonteiro@gmail.com>
2025-02-05 10:10:57 -0300

    Adicionado class BankAccount

---
+++ Models/BankAccountModel.cs
@@ -0,0 +1,25 @@
+﻿namespace PracticeClass
+{
+    public class BankAccount
+    {
+        public int numConta { get; set; }
+        public decimal saldo = 2500;
+
+        public void ExibirDetalhes()
+        {
+            Console.WriteLine($"Número conta: {numConta}");
+            Console.WriteLine($"Saldo disponível: {saldo}");
+        }
+
+        public void Deposit()
+        {
+            Console.WriteLine("Adicione saldo para sua conta:");
+            decimal depositAccount = decimal.Parse(Console.ReadLine());
+
+            decimal valorFinal = saldo + depositAccount;
+            Console.WriteLine($"O saldo disponível atualizado é de: R${valorFinal}");
+        }
+    }
+
+    
+}
--- Program.cs
+++ Program.cs
@@ -6,21 +6,33 @@ namespace PracticeClass
     {
         static void Main(string[] args)
         {
-            ModelCar carro = new ModelCar();
+            //ModelCar carro = new ModelCar();
 
-            Console.WriteLine("Digite os dados do carro:");
+            //Console.WriteLine("Digite os dados do carro:");
 
-            Console.WriteLine("Modelo do Carro:");
-            carro.nomeCarro = Console.ReadLine();
+            //Console.WriteLine("Modelo do Carro:");
+            //carro.nomeCarro = Console.ReadLine();
 
-            Console.WriteLine("Marca do carro:");
-            carro.marcaCarro = Console.ReadLine();
+            //Console.WriteLine("Marca do carro:");
+            //carro.marcaCarro = Console.ReadLine();
 
-            Console.WriteLine("Ano do carro:");
-            carro.anoCarro = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Ano do carro:");
+            //carro.anoCarro = int.Parse(Console.ReadLine());
 
 
-            carro.ShowMeDetails();
+            //carro.ShowMeDetails();
+
+
+            BankAccount x = new BankAccount();
+
+            Console.WriteLine("Digite o número da conta:");
+            x.numConta = int.Parse(Console.ReadLine());
+
+            x.Deposit();
+            x.ExibirDetalhes();
+
+
+
         }
 
     }
